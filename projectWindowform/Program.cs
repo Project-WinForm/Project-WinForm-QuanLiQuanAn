34c3590b86f913ab0bed1c337529caedb3ec56ea
Dell <2424801030091@student.tdmu.edu.vn>
2026-05-11 21:49:25 +0700

    add form login

--- projectWindowform/Program.cs
+++ projectWindowform/Program.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using projectWindowform;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using projectWindowform.GUI; // Gọi lớp Login từ GUI
 
 
 namespace projectWindowform
@@ -17,7 +19,7 @@ namespace projectWindowform
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Login()); 
         }
     }
 }
