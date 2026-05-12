81c6c933ff1dc695e82a5e130d3cc9656bd7dfb2
phucnghiengameaov <2424801030026@student.tdmu.edu.vn>
2026-05-12 10:31:08 +0700

    a

---
+++ projectWindowform/GUI/Form4.Designer.cs
@@ -0,0 +1,146 @@
+﻿namespace projectWindowform.GUI
+{
+    partial class Form4
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnLoginn = new System.Windows.Forms.Button();
+            this.txtPassW = new System.Windows.Forms.TextBox();
+            this.txtUserN = new System.Windows.Forms.TextBox();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Image = global::projectWindowform.Properties.Resources.z7816217024849_53e48889b81dfef74f8b939471d1cf5f;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1063, 604);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.Black;
+            this.label3.Location = new System.Drawing.Point(349, 43);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(394, 69);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "ĐĂNG NHẬP";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnLoginn);
+            this.groupBox1.Controls.Add(this.txtPassW);
+            this.groupBox1.Controls.Add(this.txtUserN);
+            this.groupBox1.Controls.Add(this.linkLabel1);
+            this.groupBox1.Location = new System.Drawing.Point(307, 167);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(496, 347);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            // 
+            // btnLoginn
+            // 
+            this.btnLoginn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLoginn.Location = new System.Drawing.Point(163, 235);
+            this.btnLoginn.Name = "btnLoginn";
+            this.btnLoginn.Size = new System.Drawing.Size(152, 49);
+            this.btnLoginn.TabIndex = 6;
+            this.btnLoginn.Text = "LOGIN";
+            this.btnLoginn.UseVisualStyleBackColor = true;
+            this.btnLoginn.Click += new System.EventHandler(this.btnLoginn_Click);
+            // 
+            // txtPassW
+            // 
+            this.txtPassW.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPassW.Location = new System.Drawing.Point(74, 134);
+            this.txtPassW.Name = "txtPassW";
+            this.txtPassW.Size = new System.Drawing.Size(311, 30);
+            this.txtPassW.TabIndex = 5;
+            this.txtPassW.Enter += new System.EventHandler(this.txtPassW_Enter);
+            this.txtPassW.Leave += new System.EventHandler(this.txtPassW_Leave);
+            // 
+            // txtUserN
+            // 
+            this.txtUserN.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtUserN.Location = new System.Drawing.Point(74, 56);
+            this.txtUserN.Name = "txtUserN";
+            this.txtUserN.Size = new System.Drawing.Size(311, 28);
+            this.txtUserN.TabIndex = 4;
+            this.txtUserN.Enter += new System.EventHandler(this.txtUserN_Enter);
+            this.txtUserN.Leave += new System.EventHandler(this.txtUserN_Leave);
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Location = new System.Drawing.Point(335, 184);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(116, 16);
+            this.linkLabel1.TabIndex = 3;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "Forgot Password?";
+            // 
+            // Form4
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1063, 604);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form4";
+            this.Text = "Form4";
+            this.Load += new System.EventHandler(this.Form4_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.Button btnLoginn;
+        private System.Windows.Forms.TextBox txtPassW;
+        private System.Windows.Forms.TextBox txtUserN;
+    }
+}
---
+++ projectWindowform/GUI/Form4.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using projectWindowform.BLL; // Gọi lớp BLL
+
+namespace projectWindowform.GUI
+{
+    public partial class Form4 : Form
+    {
+        public Form4()
+        {
+            InitializeComponent();
+        }
+
+        private StaffBLL staffBLL = new StaffBLL();
+
+        
+
+        private void btnLoginn_Click(object sender, EventArgs e)
+        {
+            if (staffBLL.IsAdmin(txtUserN.Text))
+            {
+                MessageBox.Show("Đăng nhập thành công với quyền quản trị!", "Thành công", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                this.Hide();
+                Form2 mainForm = new Form2();
+                mainForm.ShowDialog();
+                this.Close();
+            }
+            else if (staffBLL.Login(txtUserN.Text, txtPassW.Text))
+            {
+                MessageBox.Show("Đăng nhập thành công!", "Thành công", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                this.Hide();
+                Form1 mainForm = new Form1();
+                mainForm.ShowDialog();
+                this.Close();
+            }
+            else
+            {
+                MessageBox.Show("Tên đăng nhập hoặc mật khẩu không đúng.", "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+        }
+
+
+        // Phần load form để hiển thị placeholder cho TextBox
+        private void Form4_Load(object sender, EventArgs e)
+        {
+            txtUserN.Text = "UserName";
+            txtPassW.Text = "Password";
+
+            txtPassW.UseSystemPasswordChar = false;
+        }
+        // Các sự kiện Enter và Leave để xử lý placeholder cho TextBox
+        private void txtUserN_Enter(object sender, EventArgs e)
+        {
+            if (txtUserN.Text == "UserName")
+            {
+                txtUserN.Text = "";
+            }
+        }
+
+        private void txtUserN_Leave(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(txtUserN.Text))
+            {
+                txtUserN.Text = "UserName";
+            }
+        }
+
+        private void txtPassW_Enter(object sender, EventArgs e)
+        {
+                        if (txtPassW.Text == "Password")
+            {
+                txtPassW.Text = "";
+                txtPassW.UseSystemPasswordChar = true;
+            }
+        }
+
+        private void txtPassW_Leave(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(txtPassW.Text))
+            {
+                txtPassW.UseSystemPasswordChar = false;
+                txtPassW.Text = "Password";
+            }
+        }
+
+        
+    }
+}
