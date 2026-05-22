9efe83ad8c4021fb575e23ff4e40399b2428bbb2
LAMTAY33 <tanloc270103@gmail.com>
2026-05-22 15:37:49 +0700

    cập nhật thêm formhoadon

---
+++ projectWindowform/GUI/FormHoaDon.Designer.cs
@@ -0,0 +1,257 @@
+﻿namespace projectWindowform
+{
+    partial class FormHoaDon
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnDong = new System.Windows.Forms.Button();
+            this.btnInHoaDon = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.lblTongTien = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lsvChiTietHD = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.lblPhanCach1 = new System.Windows.Forms.Label();
+            this.lblThuNgan = new System.Windows.Forms.Label();
+            this.lblThoiGian = new System.Windows.Forms.Label();
+            this.lblTenBan = new System.Windows.Forms.Label();
+            this.lblTieuDe = new System.Windows.Forms.Label();
+            this.lblTenQuan = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.btnDong);
+            this.panel1.Controls.Add(this.btnInHoaDon);
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.lblTongTien);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.lsvChiTietHD);
+            this.panel1.Controls.Add(this.lblPhanCach1);
+            this.panel1.Controls.Add(this.lblThuNgan);
+            this.panel1.Controls.Add(this.lblThoiGian);
+            this.panel1.Controls.Add(this.lblTenBan);
+            this.panel1.Controls.Add(this.lblTieuDe);
+            this.panel1.Controls.Add(this.lblTenQuan);
+            this.panel1.Location = new System.Drawing.Point(0, 1);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(482, 653);
+            this.panel1.TabIndex = 0;
+            // 
+            // btnDong
+            // 
+            this.btnDong.BackColor = System.Drawing.Color.IndianRed;
+            this.btnDong.ForeColor = System.Drawing.Color.White;
+            this.btnDong.Location = new System.Drawing.Point(360, 609);
+            this.btnDong.Name = "btnDong";
+            this.btnDong.Size = new System.Drawing.Size(91, 31);
+            this.btnDong.TabIndex = 12;
+            this.btnDong.Text = "Đóng";
+            this.btnDong.UseVisualStyleBackColor = false;
+            this.btnDong.Click += new System.EventHandler(this.btnDong_Click);
+            // 
+            // btnInHoaDon
+            // 
+            this.btnInHoaDon.BackColor = System.Drawing.Color.CadetBlue;
+            this.btnInHoaDon.ForeColor = System.Drawing.Color.White;
+            this.btnInHoaDon.Location = new System.Drawing.Point(254, 609);
+            this.btnInHoaDon.Name = "btnInHoaDon";
+            this.btnInHoaDon.Size = new System.Drawing.Size(91, 31);
+            this.btnInHoaDon.TabIndex = 11;
+            this.btnInHoaDon.Text = "In Hóa Đơn";
+            this.btnInHoaDon.UseVisualStyleBackColor = false;
+            this.btnInHoaDon.Click += new System.EventHandler(this.btnInHoaDon_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Arial", 10.8F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(96, 565);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(290, 22);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Cảm ơn quý khách và hẹn gặp lại !";
+            // 
+            // lblTongTien
+            // 
+            this.lblTongTien.AutoSize = true;
+            this.lblTongTien.Font = new System.Drawing.Font("Arial", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTongTien.Location = new System.Drawing.Point(236, 523);
+            this.lblTongTien.Name = "lblTongTien";
+            this.lblTongTien.Size = new System.Drawing.Size(80, 27);
+            this.lblTongTien.TabIndex = 9;
+            this.lblTongTien.Text = "0 VNĐ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Arial", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(27, 523);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(163, 27);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "TỔNG CỘNG:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(27, 502);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(424, 21);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "---------------------------------------------------------------------";
+            // 
+            // lsvChiTietHD
+            // 
+            this.lsvChiTietHD.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.lsvChiTietHD.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2});
+            this.lsvChiTietHD.HeaderStyle = System.Windows.Forms.ColumnHeaderStyle.None;
+            this.lsvChiTietHD.HideSelection = false;
+            this.lsvChiTietHD.Location = new System.Drawing.Point(31, 236);
+            this.lsvChiTietHD.Name = "lsvChiTietHD";
+            this.lsvChiTietHD.Size = new System.Drawing.Size(420, 263);
+            this.lsvChiTietHD.TabIndex = 6;
+            this.lsvChiTietHD.UseCompatibleStateImageBehavior = false;
+            this.lsvChiTietHD.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Dịch Vụ";
+            this.columnHeader1.Width = 230;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Thành tiền";
+            this.columnHeader2.Width = 150;
+            // 
+            // lblPhanCach1
+            // 
+            this.lblPhanCach1.AutoSize = true;
+            this.lblPhanCach1.Font = new System.Drawing.Font("Arial", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPhanCach1.Location = new System.Drawing.Point(27, 212);
+            this.lblPhanCach1.Name = "lblPhanCach1";
+            this.lblPhanCach1.Size = new System.Drawing.Size(424, 21);
+            this.lblPhanCach1.TabIndex = 5;
+            this.lblPhanCach1.Text = "---------------------------------------------------------------------";
+            // 
+            // lblThuNgan
+            // 
+            this.lblThuNgan.AutoSize = true;
+            this.lblThuNgan.Font = new System.Drawing.Font("Arial", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblThuNgan.Location = new System.Drawing.Point(27, 191);
+            this.lblThuNgan.Name = "lblThuNgan";
+            this.lblThuNgan.Size = new System.Drawing.Size(183, 21);
+            this.lblThuNgan.TabIndex = 4;
+            this.lblThuNgan.Text = "Thu Ngân: Nhân Viên";
+            // 
+            // lblThoiGian
+            // 
+            this.lblThoiGian.AutoSize = true;
+            this.lblThoiGian.Font = new System.Drawing.Font("Arial", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblThoiGian.Location = new System.Drawing.Point(27, 159);
+            this.lblThoiGian.Name = "lblThoiGian";
+            this.lblThoiGian.Size = new System.Drawing.Size(92, 21);
+            this.lblThoiGian.TabIndex = 3;
+            this.lblThoiGian.Text = "Thời gian:";
+            // 
+            // lblTenBan
+            // 
+            this.lblTenBan.AutoSize = true;
+            this.lblTenBan.BackColor = System.Drawing.SystemColors.Window;
+            this.lblTenBan.Font = new System.Drawing.Font("Arial", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTenBan.Location = new System.Drawing.Point(27, 128);
+            this.lblTenBan.Name = "lblTenBan";
+            this.lblTenBan.Size = new System.Drawing.Size(47, 21);
+            this.lblTenBan.TabIndex = 2;
+            this.lblTenBan.Text = "Bàn:";
+            // 
+            // lblTieuDe
+            // 
+            this.lblTieuDe.AutoSize = true;
+            this.lblTieuDe.Font = new System.Drawing.Font("Arial Narrow", 13.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTieuDe.ForeColor = System.Drawing.Color.Black;
+            this.lblTieuDe.Location = new System.Drawing.Point(131, 87);
+            this.lblTieuDe.Name = "lblTieuDe";
+            this.lblTieuDe.Size = new System.Drawing.Size(195, 26);
+            this.lblTieuDe.TabIndex = 1;
+            this.lblTieuDe.Text = "Hóa Đơn Thanh Toán";
+            // 
+            // lblTenQuan
+            // 
+            this.lblTenQuan.AutoSize = true;
+            this.lblTenQuan.Font = new System.Drawing.Font("Arial Narrow", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTenQuan.Location = new System.Drawing.Point(152, 45);
+            this.lblTenQuan.Name = "lblTenQuan";
+            this.lblTenQuan.Size = new System.Drawing.Size(158, 33);
+            this.lblTenQuan.TabIndex = 0;
+            this.lblTenQuan.Text = "HANSIM 한심";
+            // 
+            // FormHoaDon
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(482, 653);
+            this.Controls.Add(this.panel1);
+            this.Name = "FormHoaDon";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "FormHoaDon";
+            this.Load += new System.EventHandler(this.FormHoaDon_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label lblPhanCach1;
+        private System.Windows.Forms.Label lblThuNgan;
+        private System.Windows.Forms.Label lblThoiGian;
+        private System.Windows.Forms.Label lblTenBan;
+        private System.Windows.Forms.Label lblTieuDe;
+        private System.Windows.Forms.Label lblTenQuan;
+        private System.Windows.Forms.ListView lsvChiTietHD;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.Button btnDong;
+        private System.Windows.Forms.Button btnInHoaDon;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label lblTongTien;
+    }
+}
---
+++ projectWindowform/GUI/FormHoaDon.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Printing;
+using System.Windows.Forms;
+using tieuluan.DTO; // Đảm bảo namespace này trùng khớp với file chứa cấu trúc dữ liệu OrderItem của bạn
+
+namespace projectWindowform
+{
+    public partial class FormHoaDon : Form
+    {
+        private string _tenBan;
+        private List<OrderItem> _listMon;
+        private int _tongTien;
+        private string _thoiGianIn; // Biến giữ cố định thời gian in, tránh bị nhảy giây khi bấm Print Preview
+
+        // Hàm khởi tạo nhận dữ liệu từ Form chính truyền sang
+        public FormHoaDon(string tenBan, List<OrderItem> listMon, int tongTien)
+        {
+            InitializeComponent();
+            this._tenBan = tenBan;
+            this._listMon = listMon;
+            this._tongTien = tongTien;
+
+            // Thiết lập cố định chuỗi thời gian ngay khi mở hóa đơn
+            this._thoiGianIn = DateTime.Now.ToString("dd/MM/yyyy HH:mm:ss");
+
+            // Ép form xuất hiện ngay chính giữa màn hình
+            this.StartPosition = FormStartPosition.CenterScreen;
+        }
+
+        private void FormHoaDon_Load(object sender, EventArgs e)
+        {
+            // Cấu hình hiển thị dòng đầy đủ cho ListView (phòng trường hợp thuộc tính Designer chưa nhận)
+            lsvChiTietHD.View = View.Details;
+            lsvChiTietHD.FullRowSelect = true;
+
+            // Gán dữ liệu chữ lên các Label thông tin chung
+            lblTenBan.Text = "Bàn: " + _tenBan;
+            lblThoiGian.Text = "Thời gian: " + _thoiGianIn;
+
+            // Đổ danh sách món ăn từ bàn vào ListView trên giao diện
+            lsvChiTietHD.Items.Clear();
+            foreach (var item in _listMon)
+            {
+                // Cột 1: Tên món kèm số lượng
+                ListViewItem lsvItem = new ListViewItem($"{item.FoodName} (x{item.Quantity})");
+
+                // Cột 2: Thành tiền của món đó
+                lsvItem.SubItems.Add(item.Total.ToString("N0") + " VNĐ");
+
+                lsvChiTietHD.Items.Add(lsvItem);
+            }
+
+            // Hiển thị tổng tiền tổng cộng lên giao diện hóa đơn
+            lblTongTien.Text = _tongTien.ToString("N0") + " VNĐ";
+        }
+
+        // Sự kiện nút ĐÓNG (Màu đỏ)
+        private void btnDong_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        // Sự kiện nút IN HÓA ĐƠN (Màu xanh) -> Bật khung xem trước bản in nhiệt
+        private void btnInHoaDon_Click(object sender, EventArgs e)
+        {
+            PrintDocument pd = new PrintDocument();
+            pd.PrintPage += new PrintPageEventHandler(PrintPage_Format);
+
+            PrintPreviewDialog ppd = new PrintPreviewDialog();
+            ppd.Document = pd;
+
+            // Cấu hình cửa sổ xem trước hiển thị cân đối
+            ppd.WindowState = FormWindowState.Normal;
+            ppd.StartPosition = FormStartPosition.CenterScreen;
+            ppd.Width = 550;
+            ppd.Height = 750;
+
+            ppd.ShowDialog();
+        }
+
+        // Logic thiết kế bố cục tờ giấy in nhiệt thực tế cho nhà hàng HANSIM
+        private void PrintPage_Format(object sender, PrintPageEventArgs e)
+        {
+            Graphics g = e.Graphics;
+
+            // Khai báo Font chữ hóa đơn chuyên dụng (Sử dụng Courier New để các ký tự căn lề thẳng hàng)
+            Font fTitle = new Font("Courier New", 16, FontStyle.Bold);
+            Font fHeader = new Font("Courier New", 12, FontStyle.Bold);
+            Font fBody = new Font("Courier New", 11, FontStyle.Regular);
+
+            float x = 40;
+            float y = 40;
+            float lineSpacing = 25;
+
+            // 1. Vẽ Tiêu đề nhà hàng
+            g.DrawString("     HANSIM 한심     ", fTitle, Brushes.Black, x, y);
+            y += 35;
+            g.DrawString("  HÓA ĐƠN THANH TOÁN  ", fHeader, Brushes.Black, x, y);
+            y += 40;
+
+            // 2. Vẽ thông tin bàn và thời gian cố định
+            g.DrawString($"Bàn: {_tenBan}", fBody, Brushes.Black, x, y);
+            y += lineSpacing;
+            g.DrawString($"Thời gian: {_thoiGianIn}", fBody, Brushes.Black, x, y);
+            y += lineSpacing;
+            g.DrawString("Thu Ngân: Nhân Viên", fBody, Brushes.Black, x, y);
+            y += 30;
+
+            // 3. Đường kẻ phân cách phần đầu
+            g.DrawString("-----------------------------------------", fBody, Brushes.Black, x, y);
+            y += 20;
+
+            // 4. Tiêu đề các cột dữ liệu
+            g.DrawString("Dịch vụ", fHeader, Brushes.Black, x, y);
+            g.DrawString("Thành tiền", fHeader, Brushes.Black, x + 210, y);
+            y += 25;
+            g.DrawString("-----------------------------------------", fBody, Brushes.Black, x, y);
+            y += 20;
+
+            // 5. Vòng lặp vẽ danh sách món ăn ra bản in
+            foreach (var item in _listMon)
+            {
+                string infoMon = $"{item.FoodName} (x{item.Quantity})";
+                string tienMon = item.Total.ToString("N0") + " VNĐ";
+
+                // Cắt chữ nếu tên món quá dài tránh đè lấn sang cột hiển thị tiền
+                if (infoMon.Length > 20)
+                {
+                    infoMon = infoMon.Substring(0, 17) + "..";
+                }
+
+                g.DrawString(infoMon, fBody, Brushes.Black, x, y);
+                g.DrawString(tienMon, fBody, Brushes.Black, x + 210, y);
+                y += lineSpacing;
+            }
+
+            // 6. Đường kẻ phân cách phần đuôi
+            g.DrawString("-----------------------------------------", fBody, Brushes.Black, x, y);
+            y += 25;
+
+            // 7. In tổng tiền thanh toán (Đẩy lề x + 210 để không bị tràn khung chữ)
+            g.DrawString("TỔNG CỘNG:", fHeader, Brushes.Black, x, y);
+            g.DrawString(_tongTien.ToString("N0") + " VNĐ", fHeader, Brushes.Black, x + 210, y);
+            y += 50;
+
+            // 8. Chân trang hóa đơn mang thông điệp nhà hàng
+            g.DrawString("   Cảm ơn quý khách và hẹn gặp lại!   ", fBody, Brushes.Black, x, y);
+        }
+    }
+}
